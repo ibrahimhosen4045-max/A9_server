496d24ea9d0b61d50a9bf96e4194e06bc93041f9
Ibrahim <ibrahimhosen4045@gmail.com>
2026-06-02 01:33:51 +0600

    add mongodb in server

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
 const express = require('express');
+const dotenv = require("dotenv")
+const cors = require("cors")
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+dotenv.config();
 const app = express();
-const port = 5500;
+app.use(cors());
+const port = process.env.PORT || 5500;
+
+
+const uri = process.env.URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+    const db = client.db("Tutor-booking")
+    const tutorList = db.collection("tutors")
+
+    app.get("/tutors", async(req, res) => {
+        const cursor = tutorList.find();
+        const result = await cursor.toArray();
+        // console.log(result)
+        res.send(result)
+    })
+
+    app.get("/tutors/:tutorsId", async(req, res) => {
+        const {tutorsId} = req.params;
+        // console.log(tutorsId)
+        const query = {_id: new ObjectId(tutorsId)}
+        const result = await tutorList.findOne(query);
+        res.send(result)
+    })
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
